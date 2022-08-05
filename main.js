8c1d1b7e173e02520652c7e48f35ddab3b8f4ac4
gozeon <goze.qiu@gmail.com>
2022-08-05 13:44:44 +0800

    feat: export png

--- main.js
+++ main.js
@@ -112,3 +112,34 @@ function addNode(e, obj, defData = {}) {
 myDiagram.model = new TreeModel([
     { key: '1', text: "Root Node" },
 ])
+
+// When the blob is complete, make an anchor tag for it and use the tag to initiate a download
+// Works in Chrome, Firefox, Safari, Edge, IE11
+function myCallback(blob) {
+    var url = window.URL.createObjectURL(blob);
+    var filename = `${Date.now()}.png`;
+
+    var a = document.createElement("a");
+    a.style = "display: none";
+    a.href = url;
+    a.download = filename;
+
+    // IE 11
+    if (window.navigator.msSaveBlob !== undefined) {
+        window.navigator.msSaveBlob(blob, filename);
+        return;
+    }
+
+    document.body.appendChild(a);
+    requestAnimationFrame(() => {
+        a.click();
+        window.URL.revokeObjectURL(url);
+        document.body.removeChild(a);
+    });
+}
+
+function makeBlob() {
+    var blob = myDiagram.makeImageData({ background: "white", returnType: "blob", callback: myCallback });
+}
+
+document.querySelector('.download').addEventListener('click', makeBlob)
